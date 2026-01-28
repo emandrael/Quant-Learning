d4976630b119fe352e0e740970b20e61abf5343b
M Raihan <57156170+emandrael@users.noreply.github.com>
2026-01-28 14:02:26 +0000

    CHAPTER 4 DONE

---
+++ CPP Beginners Guide/chapter 4/4.1 sorting array/sorting.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int size{9};
+    int nums[9]{9, 8, 76, 5, 4, 3, 2, 1, 0};
+
+    for (auto &&i : nums)
+        cout << i;
+    cout << endl;
+
+    int a, b, t;
+
+    for (a = 1; a < size; a++)
+    {
+        for (b = size - 1; b >= a; b--)
+        {
+            if (nums[b - 1] > nums[b])
+            {
+                t = nums[b - 1];
+                nums[b - 1] = nums[b];
+                nums[b] = t;
+            }
+        }
+    }
+
+    for (auto &&i : nums)
+        cout << i;
+    cout << endl;
+
+    return 0;
+}
